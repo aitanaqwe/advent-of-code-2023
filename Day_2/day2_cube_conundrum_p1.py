47da9411fa06ab18cd5da42a72a79be6c8b20c7b
CORP\abaldovi <aitana.baldovi-murcia.external@capgemini.com>
2023-12-05 16:40:32 +0100

    improved solution for d2p1

--- Day_2/day2_cube_conundrum_p1.py
+++ Day_2/day2_cube_conundrum_p1.py
@@ -2,32 +2,20 @@ import re
 
 puzzle_input = open('C:\Repos\\advent-of-code-2023\Day_2\day2_puzzle_input.txt', 'r').read().splitlines()
 
+MAX_VALUES = {'red': 12, 'green': 13, 'blue': 14}
+
 max_red = 12
 max_green = 13
 max_blue = 14
 
-game_pattern = re.compile('Game (\d+)')
-cubes_pattern = re.compile('(\d+) (\w+)')
-
 possible_games_sum = 0 
 
 def get_possible_games(game_match, cube_matches):
-    is_possible = True
     for combo in cube_matches:
-        if combo[1] == 'red':
-            if int(combo[0]) > max_red:
-                is_possible = False
-        elif combo[1] == 'green':
-            if int(combo[0]) > max_green:
-                is_possible = False
-        elif combo[1] == 'blue':
-            if int(combo[0]) > max_blue:
-                is_possible = False
-
-    if is_possible == True:
-        return int(game_match[0])
-    else:
-        return 0
+        color, value = combo[1], int(combo[0])
+        if value > MAX_VALUES.get(color):
+            return 0
+    return int(game_match[0])
 
 
 for line in puzzle_input:
